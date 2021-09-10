e6d79c321fc94e2c31d98dcf88a373c42e80a538
your_username <your_email@gmail.com>
2021-09-10 11:14:10 +0200

    Init Cra Template Test

---
+++ template/src/App.js
@@ -0,0 +1,12 @@
+import CustomTemplate from "./components/CustomComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My new Template</h1>
+      <CustomTemplate title="My Custom Template" />
+    </div>
+  );
+}
+
+export default App;
---
+++ template/src/components/CustomComponent.js
@@ -0,0 +1,3 @@
+export default function CustomTemplate(props) {
+  return <h2>{props.title}</h2>;
+}
